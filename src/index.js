edac671b66c2efd984efd4daf6dc959bfbca6e7b
devFron <developermater26@gmail.com>
2021-12-06 17:43:45 -0600

    se agrego un listener al window al cual se le asocio el eveto hashchange, cuando dicho eveto se active la callback asyncrona ejecutara la funcion App

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import styles from '../public/styles.css'
 import { App } from './App'
 document.addEventListener('DOMContentLoaded',async(e)=> await App())
+window.addEventListener('hashchange',async(e)=> await App())
