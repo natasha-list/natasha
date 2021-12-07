6c98d0fa78cf977c78da4f4057d65b3f09e8c970
devFron <developermater26@gmail.com>
2021-12-06 23:26:57 -0600

    se imporot la funcion SliderFunction que contien el fucionamiento del slider y se ejecuta despues de que la vista Home responda

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import {Home} from './pages/home/index.js'
+import { SliderFucntion } from './components/Slider.js'
 export const Router = async ()=>{
     const $Root = document.getElementById('root')
     const {hash} = location
     if(hash === '' || hash=== '#/home'){
         $Root.appendChild(await Home())
+        SliderFucntion()
     }
 }
