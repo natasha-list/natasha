e074d221c12f4a86f1294fa950e9862228bf7d1f
devFron <developermater26@gmail.com>
2021-12-06 18:25:53 -0600

    se agregaron los prefijos a usar

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,5 +1,7 @@
 import {library,icon} from '@fortawesome/fontawesome-svg-core'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {fab} from '@fortawesome/free-brands-svg-icons'
-const Icons = ({prefix,iconName})=>{}
+const Icons = ({prefix,iconName})=>{
+    library.add(fas,fab)
+}
 export default Icons
