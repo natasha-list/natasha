2742fc09d93d004702cf72f420fa97bd670aab10
devFron <developermater26@gmail.com>
2021-12-06 18:57:05 -0600

    se creo el layaout del Header, tambien se importo la fucnio Icons para poder usar iconos en la UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,19 @@
+import Icons from '../helpers/icons.js'
 export const Header = ()=>{
     const $Header = document.createElement('header')
+    $Header.innerHTML = `
+        <h1 class="logo">NATASHA</h1>
+        <nav>
+            <ul>
+                <li class="menu__item"><a href="#/home" class="menu__link">${Icons({prefix:'fas',iconName:'home'})}</a></li>
+                <li class="menu__item"><a href="#/to-do-list" class="menu__link">${Icons({prefix:'fas',iconName:'table'})}</a></li>
+                <li class="menu__item new-tas-item">${Icons({prefix:'fas',iconName:'plus-circle'})}</li>   
+            </ul>
+        </nav>
+        <section class="session__buttons">
+            <button class="session__button log-in-button">Log In</button>
+            <button class="session__button log-up-button">Log Up</button>
+        </section>
+    `
     return $Header
 }
