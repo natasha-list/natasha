bc85d57081df82649438ed7de72560ee3bf57a9f
devFron <developermater26@gmail.com>
2021-12-06 18:25:12 -0600

    se creo una funcion la cual resivira como parametro un objeto que contien las llaves 'prefix' y 'iconName' y retornara el icono segundo lo solicitado en los parametros

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,3 +1,5 @@
 import {library,icon} from '@fortawesome/fontawesome-svg-core'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {fab} from '@fortawesome/free-brands-svg-icons'
+const Icons = ({prefix,iconName})=>{}
+export default Icons
