b3a0c41484325115dba0c910446f31ff49aa5f7c
devFron <developermater26@gmail.com>
2021-12-07 12:17:49 -0600

    se importo la imagen para la seccion de download, se agreo una seccion utilizando la funcion InformationLayout que retorna un template

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import {Header} from '../../components/Header.js'
 import { InformationLayout } from '../../helpers/information_layout.js'
 import WelcomeImage from '../../assets/undraw_note_list_re_r4u9.svg'
+import DownlaodImage from '../../assets/undraw_in_sync_re_jlqd.svg'
 import { Slider} from '../../components/Slider.js'
 export const Home = async ()=>{
     const $Home = document.createElement('section')
@@ -22,5 +23,20 @@ export const Home = async ()=>{
         classFatherBox:'welcome__section',
     }))
     $Home.appendChild(Slider())
+    $Home.appendChild(InformationLayout({
+        image:{
+            url:DownlaodImage,
+            orientations:{
+                mobile:'bottom',
+                desktop:'right'
+            }
+        },
+        text:{
+            title:'Download',
+            description:'download it on the device you want'
+        },
+        extraElement:`<button class="button-download-app">Download</button>`,
+        classFatherBox:'download__section',
+    }))
     return $Home
 }
