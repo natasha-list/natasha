69076c43bcd1be8c4faaa7e22c654a2dfcf304cf
devFron <developermater26@gmail.com>
2021-12-06 23:22:06 -0600

    se agregaron los esilos para el slider de splidejs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import styles from '../public/styles.css'
+import SplideStyles from '@splidejs/splide/dist/css/themes/splide-default.min.css'
 import { App } from './App'
 document.addEventListener('DOMContentLoaded',async(e)=> await App())
 window.addEventListener('hashchange',async(e)=> await App())
