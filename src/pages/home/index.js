28eb56124de29b38a5ea1a0ea6522ecb7ceb5165
devFron <developermater26@gmail.com>
2021-12-06 19:01:07 -0600

    se imporot el componente Header el cual se agrego como un hijo de Home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,7 @@
+import {Header} from '../../components/Header.js'
 export const Home = async ()=>{
     const $Home = document.createElement('section')
     $Home.classList.add('section__home')
+    $Home.appendChild(Header())
     return $Home
 }
