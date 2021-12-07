e7f17719a4a6eb199481f2252318c818587b6af9
devFron <developermater26@gmail.com>
2021-12-06 18:11:11 -0600

    se importo la funcion Rotuer y la imagen Loader, Loader se agrego como un hijo de  el cual se removera del mismo cuando la funcion asyncrona Router responda

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import Loader from './assets/oval.svg'
+import {Router} from './router.js'
 export const App = async ()=>{
     const $Root = document.getElementById('root')
+    const $LoaderBox = document.createElement('section')
+    $LoaderBox.innerHTML = `<img src="${Loader}" alt="loader spinner" class="loader__image"> `
+    $LoaderBox.classList.add('app__loader__box')
+    $Root.appendChild($LoaderBox)
+    await Router()
+    $Root.removeChild($LoaderBox)
 }
