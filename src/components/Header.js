7dd7e068cb2121afb36039bd5d77187c36722f06
devFron <developermater26@gmail.com>
2021-12-07 12:24:09 -0600

    se seccion de los botones se movio dentro de la equite nav y se agrego el icono de hamburguesa como hijo directro de la equiqueta header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,19 @@ export const Header = ()=>{
     const $Header = document.createElement('header')
     $Header.innerHTML = `
         <h1 class="logo">NATASHA</h1>
+        <div class="hamburguer__icon">${Icons({prefix:'fas',iconName:'bars'})}</div>
         <nav>
             <ul>
                 <li class="menu__item"><a href="#/home" class="menu__link">${Icons({prefix:'fas',iconName:'home'})}</a></li>
                 <li class="menu__item"><a href="#/to-do-list" class="menu__link">${Icons({prefix:'fas',iconName:'table'})}</a></li>
                 <li class="menu__item new-tas-item">${Icons({prefix:'fas',iconName:'plus-circle'})}</li>   
             </ul>
+
+            <section class="session__buttons">
+                <button class="session__button log-in-button">Log In</button>
+                <button class="session__button log-up-button">Log Up</button>
+            </section>
         </nav>
-        <section class="session__buttons">
-            <button class="session__button log-in-button">Log In</button>
-            <button class="session__button log-up-button">Log Up</button>
-        </section>
     `
     return $Header
 }
