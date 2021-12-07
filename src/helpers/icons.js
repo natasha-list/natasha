3a7bf765ee089d07843a06983068b4b3286d4768
devFron <developermater26@gmail.com>
2021-12-06 18:26:55 -0600

    se creo el icono solicitado

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -3,5 +3,6 @@ import {fas} from '@fortawesome/free-solid-svg-icons'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 const Icons = ({prefix,iconName})=>{
     library.add(fas,fab)
+    const IconRequest = icon({prefix,iconName})
 }
 export default Icons
