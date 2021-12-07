a54618a8f0f2c9b17bb027db29d812a41636e87e
devFron <developermater26@gmail.com>
2021-12-06 22:15:25 -0600

    se importo InformationLayout para inserta un template, se le pasaraon todas sus propiedades contando la imagen importada

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,24 @@
 import {Header} from '../../components/Header.js'
+import { InformationLayout } from '../../helpers/information_layout.js'
+import WelcomeImage from '../../assets/undraw_note_list_re_r4u9.svg'
 export const Home = async ()=>{
     const $Home = document.createElement('section')
     $Home.classList.add('section__home')
     $Home.appendChild(Header())
+    $Home.appendChild(InformationLayout({
+        image:{
+            url:WelcomeImage,
+            orientations:{
+                mobile:'bottom',
+                desktop:'left'
+            }
+        },
+        text:{
+            title:'Welcome to Natasha',
+            description:'The software that will help you plan all your tasts'
+        },
+        extraElement:null,
+        classFatherBox:'welcome__section',
+    }))
     return $Home
 }
