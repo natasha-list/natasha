7748bf5a5f848f9386e51bb3c4d99c3ecf7c170d
devFron <developermater26@gmail.com>
2021-12-06 19:02:19 -0600

    se importo la vista Home y se agrega como un hijo de  cuando la validacion del hash del obejto location se a igual a nada o a '#/home'

--- src/router.js
+++ src/router.js
@@ -1,2 +1,8 @@
+import {Home} from './pages/home/index.js'
 export const Router = async ()=>{
+    const $Root = document.getElementById('root')
+    const {hash} = location
+    if(hash === '' || hash=== '#/home'){
+        $Root.appendChild(await Home())
+    }
 }
