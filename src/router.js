1bb7e9ccd93a2e14fa9759ee097b2e370bf8c3da
devFron <developermater26@gmail.com>
2021-12-07 12:04:35 -0600

    se recupero la etiqueta title la cual en la validacion del hash tomara el nombre de la seccion la cual se esta devolviendo

--- src/router.js
+++ src/router.js
@@ -2,9 +2,11 @@ import {Home} from './pages/home/index.js'
 import { SliderFucntion } from './components/Slider.js'
 export const Router = async ()=>{
     const $Root = document.getElementById('root')
+    const $Title = document.querySelector('title')
     const {hash} = location
     if(hash === '' || hash=== '#/home'){
         $Root.appendChild(await Home())
         SliderFucntion()
+        $Title.textContent = 'Home'
     }
 }
